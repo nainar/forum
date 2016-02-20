12418ec4648868e7f1c88035820ae2824f5a9a2d
Naina Raisinghani <nainar@chromium.org>
2016-02-21 10:17:54 +1100

    edit and destroy + view files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,53 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	# before_action :authenticate_user!, except: [:index, :show]
+
 	def index
+		@posts = Post.all.order("created_at DESC")
 	end
-	
+
+	def show
+	end
+
+	def new
+		@post = Post.new
+		# @post = current_user.posts.build
+	end
+
+	def create
+		@post = Post.new(post_params)
+		# @post = current_user.posts.build(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
 end
